875e89e26d34d7266970ef3b391603bcfc20bc7b
Genheden, Samuel <samuel.genheden@astrazeneca.com>
2024-08-21 09:38:02 +0200

    Update to 2.0 of pydantic

--- route_distances/validation.py
+++ route_distances/validation.py
@@ -1,24 +1,26 @@
 """ Module containing routes to validate AiZynthFinder-like input dictionaries """
 from __future__ import annotations
-from typing import Optional, List
-import pydantic
+from typing import Any, Dict, Optional, List
+from typing_extensions import Annotated
+
+from pydantic import BaseModel, StringConstraints, conlist, ValidationError
 
 from route_distances.utils.type_utils import StrDict
 
 
-class MoleculeNode(pydantic.BaseModel):
+class ReactionNode(BaseModel):
+    """Node representing a reaction"""
+
+    type: Annotated[str, StringConstraints(pattern=r"^reaction$")]
+    children: List[MoleculeNode]
+
+
+class MoleculeNode(BaseModel):
     """Node representing a molecule"""
 
     smiles: str
-    type: pydantic.constr(regex=r"^mol$")
-    children: Optional[pydantic.conlist(ReactionNode, min_items=1, max_items=1)]
-
-
-class ReactionNode(pydantic.BaseModel):
-    """Node representing a reaction"""
-
-    type: pydantic.constr(regex=r"^reaction$")
-    children: List[MoleculeNode]
+    type: Annotated[str, StringConstraints(pattern=r"^mol$")]
+    children: Optional[conlist(ReactionNode, min_length=1, max_length=1)] = None
 
 
 MoleculeNode.update_forward_refs()
@@ -31,6 +33,6 @@ def validate_dict(dict_: StrDict) -> None:
     :param dict_: the route as dictionary
     """
     try:
-        MoleculeNode(**dict_, extra=pydantic.Extra.ignore)
-    except pydantic.ValidationError as err:
+        MoleculeNode(**dict_, extra="ignore")
+    except ValidationError as err:
         raise ValueError(f"Invalid input: {err.json()}")
